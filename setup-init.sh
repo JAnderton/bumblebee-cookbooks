a879a0be139ad044477c9eb6f7bd44d10efb1054
Karun Japhet <karun@japhet.in>
2015-06-27 06:12:37 +0000

    Adding installation for git into the script

--- setup-init.sh
+++ setup-init.sh
@@ -5,19 +5,16 @@ echo ">> Checking for chef: $chefInstalled"
 if [ "" == "$chefInstalled" ]; then
 
   echo ">> No chef installation.\nDownloading version $version"
-  wget -nc https://opscode-omnibus-packages.s3.amazonaws.com/debian/6/x86_64/chef_12.4.0-1_amd64.deb
+  wget -nc https://opscode-omnibus-packages.s3.amazonaws.com/debian/6/x86_64/chef_$version.deb -O "~/chef_$version.deb"
   echo ">> Installing $version"
-  sudo dpkg -i chef_$version.deb
+  sudo dpkg -i ~/chef_$version.deb
 fi
 
-#echo ">> Installing git"
-#sudo apt-get install git
+echo ">> Installing git"
+sudo apt-get install git
 
 #echo ">> Setting up git configurations"
 #git config --global core.editor "nano"
 #git config --global user.name "Karun Japhet"
 #git config --global user.email karun@japhet.in
 #git config --global push.default simple
-
-#echo ">> Generating SSH keys"
-#ssh-keygen -t rsa -b 4096 -C "karun@japhet.in"
