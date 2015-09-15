b3d03d47db9f416336ffbf6cf24f8d2b51cd64a3
Karun Japhet <karun@japhet.in>
2015-09-15 14:17:05 +0530

    Fixing typo in recipe name which caused the runlist to bomb. Subtle miss but causes a bit PITA

--- roles/webserver.rb
+++ roles/webserver.rb
@@ -1,6 +1,8 @@
 name "webserver"
 description "Chef role for all webserver machines"
-run_list "recipe[ngnix]"
+run_list [
+  "recipe[nginx]"
+]
 override_attributes({
   "starter_name" => "Karun Japhet",
 })
