8aa8f1460b00c04dbdf23ab821d6be60adf1492e
Karun Japhet <karun@japhet.in>
2015-11-15 22:11:19 +0530

    Updating knife config with new chef_server_url

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,4 +1,4 @@
-# See https://docs.chef.io/config_rb_knife.html for more information on knife configuration options
+# See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
 
 current_dir = File.dirname(__FILE__)
 log_level                :info
@@ -7,5 +7,5 @@ node_name                "karun"
 client_key               "#{current_dir}/karun.pem"
 validation_client_name   "prorthymix-validator"
 validation_key           "#{current_dir}/prorthymix-validator.pem"
-chef_server_url          "https://api.opscode.com/organizations/prorthymix"
+chef_server_url          "https://api.chef.io/organizations/prorthymix"
 cookbook_path            ["#{current_dir}/../cookbooks"]
