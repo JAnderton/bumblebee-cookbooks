d4a90360a280fff0482638d1439ffb785b6fa2b3
Karun Japhet <karun@japhet.in>
2015-11-23 00:09:46 +0530

    Updating run list for role that was missed. oops..

--- roles/webserver.rb
+++ roles/webserver.rb
@@ -5,13 +5,16 @@ name "webserver"
 description "Chef role for all webserver machines"
 
 run_list(
-  "recipe[nginx]",
   "recipe[hostname]",
-  "recipe[git]",
+  "recipe[nginx]",
   "recipe[trion-cookbooks::disable_sites]",
   "recipe[trion-cookbooks::setup_www_root]",
+  "recipe[git]",
   "recipe[trion-cookbooks::deploy_default_site]",
   "recipe[trion-cookbooks::deploy_japhet_in]",
   "recipe[trion-cookbooks::deploy_tributetothebeast_com]",
   "recipe[trion-cookbooks::deploy_prorthymix_com]",
+  "recipe[trion-cookbooks::install_octopress]",
+  "recipe[nodejs]",
+  "recipe[trion-cookbooks::deploy_blog_karun_me]"
 )
