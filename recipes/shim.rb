fac36a1c60aec38d781114a6390d2192129a325d
Karun Japhet <karun@japhet.in>
2016-02-21 16:29:45 -0500

    Moving recipes to be called to a shim file to make management of order and execution centrally between kitchen and roles

---
+++ recipes/shim.rb
@@ -0,0 +1,35 @@
+#
+# Cookbook Name:: trion-cookbooks
+# Recipe:: default
+#
+# Copyright (C) 2015 Karun Japhet
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "hostname"
+include_recipe "trion::install_httpd"
+# include_recipe "trion::disable_sites"
+# include_recipe "trion::setup_www_root"
+# include_recipe "git"
+# include_recipe "trion::deploy_default_site"
+# include_recipe "trion::deploy_japhet_in"
+# include_recipe "trion::deploy_tributetothebeast_com"
+# include_recipe "trion::deploy_prorthymix_com"
+# include_recipe "build-essential"
+# include_recipe "trion::install_octopress"
+# include_recipe "nodejs"
+# include_recipe "trion::deploy_blog_karun_me"
+# include_recipe "trion::deploy_karunab_com"
+# include_recipe "trion::deploy_karun_me"
+# include_recipe "trion::deploy_karun_japhet_in"
--- roles/webserver.rb
+++ roles/webserver.rb
@@ -5,20 +5,5 @@ name "webserver"
 description "Chef role for all webserver machines"
 
 run_list(
-  "recipe[hostname]",
-  "recipe[nginx]",
-  "recipe[trion::disable_sites]",
-  "recipe[trion::setup_www_root]",
-  "recipe[git]",
-  "recipe[trion::deploy_default_site]",
-  "recipe[trion::deploy_japhet_in]",
-  "recipe[trion::deploy_tributetothebeast_com]",
-  "recipe[trion::deploy_prorthymix_com]",
-  "recipe[build-essential]",
-  "recipe[trion::install_octopress]",
-  "recipe[nodejs]",
-  "recipe[trion::deploy_blog_karun_me]",
-  "recipe[trion::deploy_karunab_com]",
-  "recipe[trion::deploy_karun_me]",
-  "recipe[trion::deploy_karun_japhet_in]"
+  "recipe[trion::shim]",
 )
